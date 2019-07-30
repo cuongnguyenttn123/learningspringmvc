dd0aab212940d1949b8640ca03aa9a444fe83bc5
cuong.nguyenvan <cuong.codecomplete@gmail.com>
2019-07-30 10:04:34 +0700

    first_project

---
+++ src/main/java/cuong/tt/config/SpringWebConfig.java
@@ -0,0 +1,20 @@
+package cuong.tt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+@EnableWebMvc // mvc:annotation-driven
+@Configuration
+@ComponentScan({ "cuong.tt" })
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
